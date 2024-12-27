c33f5d7a17cfb9f8b09910ecfa3dd486f314263b
João Figueiredo <joao.ggwp@outlook.com>
2024-12-26 22:28:33 -0300

    feat: create class to decouple do class validator, create interface, dinamic errors and fields

---
+++ @shared/domain/validators/validator-fields-interface.ts
@@ -0,0 +1,11 @@
+
+export type FieldsErrors = {
+    [field: string]: string[];
+}
+
+export interface IValidatorFields<PropsValidated> {
+  validate(data: any): boolean;
+  errors: FieldsErrors | null;
+  validatedData: PropsValidated | null;
+  
+}
