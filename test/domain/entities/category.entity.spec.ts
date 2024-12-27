536ee51ea1e6e82f52e643e5df078f649770ba05
João Figueiredo <joao.ggwp@outlook.com>
2024-12-26 22:25:37 -0300

    feat: create tests to uuid vo, vo and add tests to category entity

--- test/domain/entities/category.entity.spec.ts
+++ test/domain/entities/category.entity.spec.ts
@@ -1,10 +1,16 @@
+import Uuid from "../../../@shared/domain/value-objects/uuid.vo";
 import { Category } from "../../../src/domain/entities/category.entity";
 
 describe("CategotyEntity", () => {
+  let validateSpy: any;
+  beforeEach(() => {
+    validateSpy = jest.spyOn(Category, "validate");
+  });
   test("Should be create a new Categoty", () => {
     const categoryCreated: Category = new Category({
-      id: "1234-5678-91011",
+      id: new Uuid(),
       name: "Fic",
+      is_active: true,
     });
     const categoryMethod = Category.create(categoryCreated);
     expect(categoryMethod).toStrictEqual(categoryCreated);
@@ -12,27 +18,30 @@ describe("CategotyEntity", () => {
 
   test("Should be get entity Id", () => {
     const category: Category = new Category({
-      id: "1234-5678-91011",
+      id: new Uuid(),
       name: "Fic",
+      is_active: true,
     });
 
     expect(category.entity_id).toStrictEqual(category.id);
   });
 
   test("Should be changeName", () => {
-    const category: Category = new Category({
-      id: "1234-5678-91011",
+    const category: Category = Category.create({
       name: "Fic",
+      is_active: true,
     });
     category.changeName("Terror");
     expect(category.name).toStrictEqual("Terror");
+    expect(validateSpy).toHaveBeenCalledTimes(2);
   });
 
   test("Should be changeDescription", () => {
     const category: Category = new Category({
-      id: "1234-5678-91011",
+      id: new Uuid(),
       name: "Fic",
       description: "Any",
+      is_active: true,
     });
     category.changeDescription("Very Great");
     expect(category.description).toStrictEqual("Very Great");
@@ -40,7 +49,7 @@ describe("CategotyEntity", () => {
 
   test("Should be activate", () => {
     const category: Category = new Category({
-      id: "1234-5678-91011",
+      id: new Uuid(),
       name: "Fic",
       is_active: false,
     });
@@ -49,7 +58,7 @@ describe("CategotyEntity", () => {
   });
   test("Should be deactivate", () => {
     const category: Category = new Category({
-      id: "1234-5678-91011",
+      id: new Uuid(),
       name: "Fic",
       is_active: true,
     });
@@ -59,16 +68,24 @@ describe("CategotyEntity", () => {
 
   test("Should be return JSON Format", () => {
     const category: Category = new Category({
-      id: "1234-5678-91011",
+      id: new Uuid(),
       name: "Fic",
       is_active: true,
     });
     expect(category.toJSON()).toStrictEqual({
-      id: "1234-5678-91011",
+      id: category.id,
       name: "Fic",
       is_active: true,
-      created_at: undefined,
-      description: undefined,
+      created_at:category.created_at,
+      description: null,
     });
   });
 });
+
+describe("categoryValidator" , () => {
+  test("create command" , () => {
+    const category = Category.create({
+      name: ""
+    })
+  })
+})
---
+++ test/domain/entities/value-object.spec.ts
@@ -0,0 +1,15 @@
+import ValueObject from "../../../@shared/domain/value-objects/value-object";
+
+describe("ValeuObject", () => {
+  test("Should be verify if vo is null or undefined", () => {
+    const valueObject = new ValueObject();
+    const result = valueObject.equals(null);
+    expect(result).toBeFalsy();
+  });
+  test("Should be return false if constructor name vo if not equais this", () => {
+    class Cpf extends ValueObject {}
+    const instance = new Cpf();
+    const result = instance.equals(instance);
+    expect(result).toBeTruthy();
+  });
+});
