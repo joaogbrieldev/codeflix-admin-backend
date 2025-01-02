6a524b04d6e9832f1f5a912a1ebf31dd2fe0f674
João Figueiredo <joao.ggwp@outlook.com>
2025-01-02 20:06:48 +0000

    fix: change generic error to personalizated error and fix some repositories methods

--- @shared/src/domain/contracts/infra/repository/repository-base.ts
+++ @shared/src/domain/contracts/infra/repository/repository-base.ts
@@ -1,9 +1,14 @@
 import ValueObject from "../../../value-objects/value-object";
-import { IEntityBase } from "../../entity/entity-base";
+import IEntityBase from "../../entity/entity-base";
 
-export default abstract class IRepositoryBase<DomainModel extends IEntityBase, EntityId extends ValueObject> {
+export default abstract class IRepositoryBase<
+  DomainModel extends IEntityBase,
+  EntityId extends ValueObject
+> {
   abstract create(entity: DomainModel): Promise<void>;
-  abstract update(id: EntityId, entity: DomainModel): Promise<DomainModel | null>;
+  abstract update(
+    entity: DomainModel
+  ): Promise<void>;
   abstract delete(id: EntityId): Promise<void>;
   abstract findById(id: EntityId): Promise<DomainModel | null>;
   abstract getAll(): Promise<DomainModel[]>;
--- @shared/src/infra/db/in-memory/in-memory.repository.ts
+++ @shared/src/infra/db/in-memory/in-memory.repository.ts
@@ -1,23 +1,42 @@
-import { IEntityBase } from "../../../domain/contracts/entity/entity-base";
+
+import IEntityBase from "../../../domain/contracts/entity/entity-base";
 import IRepositoryBase from "../../../domain/contracts/infra/repository/repository-base";
+import { NotFoundError } from "../../../domain/errors/not-found.error";
 import ValueObject from "../../../domain/value-objects/value-object";
 
-export class InMemoryRepository<DomainModel extends IEntityBase, EntityId extends ValueObject> extends IRepositoryBase<DomainModel, EntityId>{
+
+export abstract class InMemoryRepository<DomainModel extends IEntityBase, EntityId extends ValueObject> extends IRepositoryBase<DomainModel, EntityId>{
   protected model: DomainModel[] = [];
 
   async create(entity: DomainModel): Promise<void>{
     this.model.push(entity)
   };
-  async update(id: EntityId, entity: DomainModel): Promise<DomainModel | null>{
-    throw new Error('method not implement')
+  async update(entity: DomainModel): Promise<void>{
+    const indexFound = this.model.findIndex((item) => item.entity_id.equals(entity.entity_id))
+    if (indexFound === -1){
+      throw new NotFoundError(entity.entity_id, this.getEntity())
+    }
+    this.model[indexFound] = entity
+
   };
-  async delete(id: EntityId): Promise<void>{
-    const indexFound = this.model.findIndex((item) => item.entity_id.equals(id))
+  async delete(entity_id: EntityId): Promise<void>{
+    const indexFound = this.model.findIndex((item) => item.entity_id.equals(entity_id))
+    if (indexFound === -1){
+      throw new NotFoundError(entity_id, this.getEntity())
+    }
+    this.model.splice(indexFound, 1);
   };
-  async findById(id: EntityId): Promise<DomainModel | null>{
-    throw new Error('method not implement')
+  async findById(entity_id: EntityId): Promise<DomainModel | null>{
+    return this._get(entity_id)
   };
   async getAll(): Promise<DomainModel[]>{
     return this.model;
   };
+
+  protected _get(entity_id: EntityId){
+    const item = this.model.find((item) => item.entity_id.equals(entity_id))
+    return typeof item === "undefined" ? null : item;
+  }
+
+  abstract getEntity(): new (...args: any[]) => DomainModel;
 }
