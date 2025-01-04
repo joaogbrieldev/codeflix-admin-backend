cc12006f20def7a3904f26276c58a3567ad059bf
João Figueiredo <joao.ggwp@outlook.com>
2025-01-04 11:21:33 -0300

    feat: Create new class to `inMemoryRepository` implements to `SearchableRepository`
      with tests

--- @shared/src/domain/models/entities/entity-base.ts
+++ @shared/src/domain/models/entities/entity-base.ts
@@ -1,8 +1,7 @@
 import IEntityBase from "../../contracts/entity/entity-base";
-import { Uuid } from "../../value-objects/uuid.vo";
+import { ValueObject } from "../../value-objects/value-object";
 
-export class EntityBase implements IEntityBase {
-  id?: Uuid;
-  createdAt?: Date;
-  updatedAt?: Date;
+export abstract class EntityBase implements IEntityBase {
+  abstract get entity_id(): ValueObject;
+  abstract toJSON(): any;
 }
--- @shared/src/infra/db/in-memory/in-memory.repository.ts
+++ @shared/src/infra/db/in-memory/in-memory.repository.ts
@@ -1,6 +1,8 @@
 import IEntityBase from "../../../domain/contracts/entity/entity-base";
 import IRepositoryBase from "../../../domain/contracts/infra/repository/repository-base";
 import { NotFoundError } from "../../../domain/errors/not-found.error";
+import { EntityBase } from "../../../domain/models/entities/entity-base";
+import { Uuid } from "../../../domain/value-objects/uuid.vo";
 import { ValueObject } from "../../../domain/value-objects/value-object";
 
 export abstract class InMemoryRepository<
@@ -41,4 +43,74 @@ export abstract class InMemoryRepository<
   }
 
   abstract getEntity(): new (...args: any[]) => DomainModel;
+}
+
+export abstract class InMemorySearchableRepository<
+    DomainModel extends IEntityBase,
+    EntityId extends Uuid,
+    Filter = string
+  >
+  extends InMemoryRepository<DomainModel, EntityId>
+  implements ISearchableRepository<DomainModel, EntityId, Filter>
+{
+  sortableFields: string[] = [];
+  async search(props: SearchParams<Filter>): Promise<SearchResult<EntityBase>> {
+    const itemsFiltered = await this.applyFilter(this.model, props.filter);
+    const itemsSorted = this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sort_dir
+    );
+    const itemsPaginate = this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.per_page
+    );
+    return new SearchResult({
+      items: itemsPaginate,
+      total: itemsFiltered.length,
+      current_page: props.page,
+      per_page: props.per_page,
+    });
+  }
+
+  protected abstract applyFilter(
+    items: EntityBase[],
+    filter: Filter | null
+  ): Promise<EntityBase[]>;
+
+  protected applyPaginate(
+    items: EntityBase[],
+    page: SearchParams["page"],
+    per_page: SearchParams["per_page"]
+  ) {
+    const start = (page - 1) * per_page; // 0 * 15 = 0
+    const limit = start + per_page; // 0 + 15 = 15
+    return items.slice(start, limit);
+  }
+
+  protected applySort(
+    items: EntityBase[],
+    sort: string | null,
+    sort_dir: SortDirection | null,
+    custom_getter?: (sort: string, item: EntityBase) => any
+  ) {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items;
+    }
+
+    return [...items].sort((a, b) => {
+      const aValue = custom_getter ? custom_getter(sort, a) : a[sort];
+      const bValue = custom_getter ? custom_getter(sort, b) : b[sort];
+      if (aValue < bValue) {
+        return sort_dir === "asc" ? -1 : 1;
+      }
+
+      if (aValue > bValue) {
+        return sort_dir === "asc" ? 1 : -1;
+      }
+
+      return 0;
+    });
+  }
 }
