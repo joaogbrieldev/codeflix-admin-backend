89f4c7cca6590dfac19d1c220a38a3d0074f23de
João Figueiredo <joao.ggwp@outlook.com>
2025-01-11 13:26:34 -0300

    fix: change input findById use case and create test

--- src/data/use-cases/category/findById/findById-category.use-case.ts
+++ src/data/use-cases/category/findById/findById-category.use-case.ts
@@ -1,19 +1,26 @@
+import { NotFoundError } from "@shared/src/domain/errors/not-found.error";
 import { ICategoryRepository } from "src/domain/contracts/repositories/category.repository";
-import { IFindByIdCategoryUseCase, IFindByIdInputUseCase, IFindByIdOutputUseCase } from "src/domain/contracts/use-cases/category/findById/findById-category";
+import {
+  IFindByIdCategoryUseCase,
+  IFindByIdInputUseCase,
+  IFindByIdOutputUseCase,
+} from "src/domain/contracts/use-cases/category/findById/findById-category";
+import { Category, CategoryId } from "src/domain/entities/category.entity";
 
 export class FindByIdCategoryUseCase implements IFindByIdCategoryUseCase {
-  constructor(private readonly _categoryRepository: ICategoryRepository){}
+  constructor(private readonly _categoryRepository: ICategoryRepository) {}
   async execute(input: IFindByIdInputUseCase): Promise<IFindByIdOutputUseCase> {
-    const entity = await this._categoryRepository.findById(input.id);
-    if (!entity){
-      throw new Error('Category Not Found!')
+    const categoryId = new CategoryId(input.id);
+    const entity = await this._categoryRepository.findById(categoryId);
+    if (!entity) {
+      throw new NotFoundError(input.id, Category);
     }
     return {
-        id: entity.category_id.id,
-        name: entity.name,
-        description: entity.description,
-        is_active: entity.is_active,
-        created_at: entity.created_at
-   }
+      id: entity.category_id.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at,
+    };
   }
 }
