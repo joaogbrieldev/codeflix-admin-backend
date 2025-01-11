953a092f5b35396e1405ad057a3efdaf496c69e2
João Figueiredo <joao.ggwp@outlook.com>
2025-01-11 13:27:42 -0300

    fix: add test to delete category and fix some use cases and tests

--- src/data/use-cases/category/update/update-category.use-case.ts
+++ src/data/use-cases/category/update/update-category.use-case.ts
@@ -1,19 +1,42 @@
+import { NotFoundError } from "@shared/src/domain/errors/not-found.error";
 import { ICategoryRepository } from "src/domain/contracts/repositories/category.repository";
-import { IUpdateCategoryInput, IUpdateCategoryUseCase } from "src/domain/contracts/use-cases/category/update/update-category";
-import { Category } from "src/domain/entities/category.entity";
+import {
+  IUpdateCategoryInput,
+  IUpdateCategoryOutput,
+  IUpdateCategoryUseCase,
+} from "src/domain/contracts/use-cases/category/update/update-category";
+import { Category, CategoryId } from "src/domain/entities/category.entity";
 
-export class UpdateCategoryUseCase implements IUpdateCategoryUseCase{
-  constructor(private readonly _categoryRepository: ICategoryRepository){};
-  async execute(input: IUpdateCategoryInput): Promise<void> {
-    const entity = await this._categoryRepository.findById(input.id);
-    if (!entity){
-      throw new Error('Category Not Found!')
+export class UpdateCategoryUseCase implements IUpdateCategoryUseCase {
+  constructor(private readonly _categoryRepository: ICategoryRepository) {}
+  async execute(input: IUpdateCategoryInput): Promise<IUpdateCategoryOutput> {
+    const categoryId = new CategoryId(input.id);
+    const category = await this._categoryRepository.findById(categoryId);
+    if (!category) {
+      throw new NotFoundError(input.id, Category);
     }
-    await this._categoryRepository.update(new Category({
-      category_id: input.id,
-      name: input.name,
-      description: input.description,
-      is_active: input.is_active,
-    }))
+    input.name && category.changeName(input.name);
+
+    if ("description" in input) {
+      category.changeDescription(input.description || null);
+    }
+
+    if (input.is_active === true) {
+      category.activate();
+    }
+
+    if (input.is_active === false) {
+      category.deactivate();
+    }
+
+    await this._categoryRepository.update(category);
+
+    return {
+      id: category.category_id,
+      name: category.name,
+      description: category.description || null,
+      is_active: category.is_active,
+      created_at: category.created_at,
+    };
   }
 }
