5660353cdacd72ec58ed4abad8cdbcc9e2c98760
João Figueiredo <joao.ggwp@outlook.com>
2025-02-02 12:35:30 +0000

    feat: create helper to using in tests e2e

--- src/test/e2e/categories/create-category.e2e-spec.ts
+++ src/test/e2e/categories/create-category.e2e-spec.ts
@@ -1,35 +1,30 @@
-import { INestApplication } from "@nestjs/common";
-import { Test, TestingModule } from "@nestjs/testing";
-import { AppModule } from "src/app.module";
-import { ICategoryRepository } from "src/core/domain/contracts/repositories/category.repository";
-import { CreateCategoryFixture } from "src/nest-modules/categories-module/__tests__/category.fixture";
-import { CATEGORY_PROVIDERS } from "src/nest-modules/categories-module/categories.providers";
-import { applyGlobalConfig } from "src/nest-modules/global.config";
+import { ICategoryRepository } from 'src/core/domain/contracts/repositories/category.repository';
+import { CreateCategoryFixture } from 'src/nest-modules/categories-module/__tests__/category.fixture';
+import { CATEGORY_PROVIDERS } from 'src/nest-modules/categories-module/categories.providers';
+import { startApp } from 'src/nest-modules/shared-module/testing/helpers';
 import request from 'supertest';
 
 describe('CategoriesController e2e', () => {
-  let app: INestApplication;
+  const appHelper = startApp();
   let categoryRepo: ICategoryRepository;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule]
-    }).compile()
-
-    app = moduleFixture.createNestApplication()
-    applyGlobalConfig(app);
-    await app.init()
-    categoryRepo = app.get<ICategoryRepository>(CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide)
-  })
+    categoryRepo = appHelper.app.get<ICategoryRepository>(
+      CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+    );
+  });
 
   describe('/categories (POST)', () => {
     const arrange = CreateCategoryFixture.arrangeForCreate();
 
-    test.each(arrange)('when body in $send_data', async ({send_data, expected}) => {
-      const res = await request(app.getHttpServer()).post('/categories').send(send_data).expect(201)
-      console.log(res)
-      
-    })
-  })
-
-})
+    test.each(arrange)(
+      'when body in $send_data',
+      async ({ send_data, expected }) => {
+        const res = await request(app.getHttpServer())
+          .post('/categories')
+          .send(send_data)
+          .expect(201);
+      },
+    );
+  });
+});
